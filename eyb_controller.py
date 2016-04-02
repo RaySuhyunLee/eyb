74c12482469fa654d035a26c1a691a268ea4a1e5
이수현 <SuhyunLee@YasuBook.local>
2016-04-02 15:31:52 +0900

    Show image

--- eyb_controller.py
+++ eyb_controller.py
@@ -1,11 +1,18 @@
 import sys
+import os
 from PyQt4 import QtCore, QtGui
 
-class MyWindow(QtGui.QWidget):
+class MyWindow(QtGui.QMainWindow):
     def __init__(self):
-        QtGui.QWidget.__init__(self)
+        QtGui.QMainWindow.__init__(self)
 
-        self.setWindowTitle("Emergency!")
+        self.centralWidget = QtGui.QWidget(self)
+        self.centralWidget.setGeometry(0, 0, 2000, 1500)
+        
+	self.pic = QtGui.QLabel(self.centralWidget)
+	self.pic.setGeometry(0, 0, 2000, 1500)
+	#use full ABSOLUTE path to the image, not relative
+	self.pic.setPixmap(QtGui.QPixmap(os.getcwd() + "/image.png"))
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
